f97ec7810ad635b881d6c6d85a037e7eb1bb00e7
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-04-29 16:22:16 +0800

    update: added user to decision model

--- api/models/decision.py
+++ api/models/decision.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Decision(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="decisions")
+
     title = models.CharField(max_length=255)
     context = models.TextField()
     chosen_action = models.TextField()
