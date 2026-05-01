7a9dfa60f3c0f17cf4867fa857538fdc39e287e9
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-05-01 15:42:00 +0800

    update: secured new secret key

--- ethos/settings.py
+++ ethos/settings.py
@@ -24,8 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*$dwna2b1^t)fq!u0o+=7(%yax1b$wnph$etjxlqz-wkv(u)p3'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+# old secret key: 'django-insecure-*$dwna2b1^t)fq!u0o+=7(%yax1b$wnph$etjxlqz-wkv(u)p3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
