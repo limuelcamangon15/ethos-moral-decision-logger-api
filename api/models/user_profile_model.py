85f9733b3dcad7083ba4028728d4d96213e65269
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-05-01 16:24:18 +0800

    update: deleted user profile for now

--- api/models/user_profile_model.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    email = models.EmailField(unique=True)
-    first_name = models.TextField()
-    last_name = models.TextField()
